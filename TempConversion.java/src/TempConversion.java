4867f5da08d6ea14eb3cb4e7c1e395979a95a611
mElkin00 <masonelkin@proton.me>
2026-02-03 10:01:32 -0700

    FtoCK

---
+++ TempConversion.java/src/TempConversion.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TempConversion {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		double F, C, K;
+
+		System.out.println("What is the Temperature in Fahrenheit?");
+		F = input.nextDouble();
+		//Fahrenheit is (F - 32) times (5/9)
+		C = (F - 32) * 5 /9;
+		//Kelvin is Celcius + 273.15
+		K = C + + 273.15;
+		System.out.printf("It is %.2f Celcius and %.2f Kelvin when it is %.2f Fahrenheit", C, K, F);
+		
+	}
+}

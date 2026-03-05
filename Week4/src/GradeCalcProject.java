eba027ac35e48a87045852a1f53c1763930f7933
mElkin00 <masonelkin@proton.me>
2026-03-05 10:15:39 -0700

    gradeCalculatorUpdated

--- Week4/src/GradeCalcProject.java
+++ Week4/src/GradeCalcProject.java
@@ -29,7 +29,7 @@ public class GradeCalcProject {
 				}
 			}
 			
-			else (grade >= 80 && grade < 90) {
+			else if (grade >= 80 && grade < 90) {
 				letter = "B";
 				if (grade >= 87) {
 					symbol = "+";
@@ -39,7 +39,7 @@ public class GradeCalcProject {
 				}
 			}
 			
-			else (grade >= 70 && grade < 80) {
+			else if (grade >= 70 && grade < 80) {
 				letter = "C";
 				if (grade >= 77) {
 					symbol = "+";
@@ -49,13 +49,13 @@ public class GradeCalcProject {
 				}
 			}
 			
-			else (grade >= 60 && grade < 70) {
+			else if (grade >= 60 && grade < 70) {
 				letter = "D";
 				if (grade >= 67) {
 					symbol = "+";
 				}
 			}
-			else (grade < 60) {
+			else if (grade < 60) { 
 				letter = "F";
 			}
 			

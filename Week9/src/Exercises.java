7f3cbe7d2b347b1043b768cb030f77ce902b605e
mElkin00 <masonelkin@proton.me>
2026-03-23 10:33:38 -0600

    week9

---
+++ Week9/src/Exercises.java
@@ -0,0 +1,14 @@
+
+public class Exercises {
+
+	public static void main(String[] args) {
+		boolean bool = true;
+		char ltr = 'a';
+		int num = 10;
+		double dbl = 1.2;
+		String word = "hello";
+		System.out.println("" + bool + bool);
+
+	}
+
+}

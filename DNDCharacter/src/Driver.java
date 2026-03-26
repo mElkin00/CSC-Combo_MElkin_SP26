3da7a55dcdfb4d24ffb046147defc72f66d0bcf1
Mason Elkin <masonelkin@proton.me>
2026-03-26 10:54:39 -0600

    dnd

--- DNDCharacter/src/Driver.java
+++ DNDCharacter/src/Driver.java
@@ -3,12 +3,12 @@ public class Driver {
 
 	public static void main(String[] args) {
 		Person hero = new Person();
-		hero.setHealth(100);
+		hero.setHealth(1000);
 		hero.setStrength(25);
 		hero.setName("Batman");
 		hero.setWeapon("Batarang", 15);
 		System.out.println(hero);
-		Person villan = new Person(100, 35, "The Joker", "Cane", 10);
+		Person villan = new Person(1000, 35, "The Joker", "Cane", 10);
 		System.out.println(villan);
 		
 		while(hero.getHealth() > 0 && villan.getHealth() > 0) {
@@ -17,12 +17,12 @@ public class Driver {
 			if(hero.getHealth() > 0) {
 				hDamage = hero.hit(villan);
 				System.out.print(hero.getName() + " hits " + villan.getName() + " for " + hDamage + " damage ");
-				//hero.hit(villan);
+				
 			}
 			if(villan.getHealth() > 0) {
 				vDamage = villan.hit(hero);
 				System.out.print(villan.getName() + " hits " + hero.getName() + " for " + vDamage + " damage ");
-				//villan.hit(hero);
+				
 			}
 			
 

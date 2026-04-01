3e6de939d81779dbecb1f171d2933806fa1d5cee
mElkin00 <masonelkin@proton.me>
2026-04-01 09:16:55 -0600

    cardGames

--- CardGames/src/Card.java
+++ CardGames/src/Card.java
@@ -1,17 +1,24 @@
 
 public class Card {
-	private int suit;
-	private int rank;
+	//final means that these instance variables immutable
+	//private means that no one outside of this script can see it
+	private final int suit;
+	private final int rank;
 
+	//static that everyone comes here to see what is in the array
 	private static final String[] SUITS = { "Clubs", "Diamonds", "Hearts", "Spades" };
+	//the null at oth position is so an ace is 1 , deuse is a 2...
 	private static final String[] RANKS = { null, "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen",
 			"King" };
 
+	//constructor takes a rank and suit (ints) to create a card
 	public Card(int rank, int suit) {
 		this.rank = rank;
 		this.suit = suit;
 	}
 	
+	//we create an equals method to check both instance vars
+	//we cant check a tuple we have to check each value
 	public boolean equals(Card that) {
 		return this.suit == that.suit && this.rank == that.rank;
 	}

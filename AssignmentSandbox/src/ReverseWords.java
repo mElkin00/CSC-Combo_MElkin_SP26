16bfb92b21711db494b9f3ee6d2ca2c0cc617eac
Mason Elkin <masonelkin@proton.me>
2026-04-02 13:59:00 -0600

    classAssignments

--- AssignmentSandbox/src/ReverseWords.java
+++ AssignmentSandbox/src/ReverseWords.java
@@ -7,9 +7,25 @@ public class ReverseWords {
 		Scanner input = new Scanner(System.in);
 		System.out.println("How many words do you want to sort?");
 		int numWords = input.nextInt();
+		int count = numWords;
+		String word = "hihihih";
+		String[] words = new String[] {};
+		
 		
 		//probably make a counter for numWords to use a while loop
 		//so just subtract one from the counter until we exit the loop
+		
+		while(count != 0) {
+			System.out.print("Enter in your word: ");
+			word = input.next();
+			
+			
+			
+			
+			System.out.println(count);
+			System.out.println(words[0]);
+			count--;
+		}
 		
 		String fruit = "banana";
 		char letter = fruit.charAt(0);

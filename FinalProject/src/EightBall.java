61dcd347c845090f34a9a813afc8aee51c6de02d
Mason <masonelkin@proton.me>
2026-05-04 21:23:30 -0600

    finalProject

--- FinalProject/src/EightBall.java
+++ FinalProject/src/EightBall.java
@@ -3,35 +3,37 @@ import java.util.Random;
 public class EightBall {
 
 	public static void main(String[] args) {
-		Random rand = new Random();
 		
-		String[] negs = {"Don't count on it", "My reply is no", "My sources say no",
-				"Outlook not so good", "Very doubtful"};
+//		Random rand = new Random();
+//		
+//		String[] negs = {"Don't count on it", "My reply is no", "My sources say no",
+//				"Outlook not so good", "Very doubtful"};
+//		
+//		String[] ok = {"Reply hazy, try again", "Ask again later", "Better not tell you now",
+//				"Cannot predict now", "Concentrate and ask again"};
+//		
+//		String[] pos = {"It is certain", "it is decidedly so",
+//				"Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes",
+//				"Most likely", "Outlook good", "Yes", "Signs point to yes"};
+//
+//		if(rand.nextInt(3) == 0) {
+//			System.out.println("Negative response");
+//			int num = rand.nextInt(5);
+//			System.out.println(negs[num]);
+//		}
+//		
+//		else if(rand.nextInt(3) == 1) {
+//			System.out.println("Ok response");
+//			int num = rand.nextInt(7);
+//			System.out.println(ok[num]);
+//		}
+//		
+//		else {
+//			System.out.println("Postitive response");
+//			int num = rand.nextInt(10);
+//			System.out.println(pos[num]);
+//		}
 		
-		String[] ok = {"Reply hazy, try again", "Ask again later", "Better not tell you now",
-				"Cannot predict now", "Concentrate and ask again"};
-		
-		String[] pos = {"It is certain", "it is decidedly so",
-				"Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes",
-				"Most likely", "Outlook good", "Yes", "Signs point to yes"};
-
-		if(rand.nextInt(3) == 0) {
-			System.out.println("Negative response");
-			int num = rand.nextInt(5);
-			System.out.println(negs[num]);
-		}
-		
-		else if(rand.nextInt(3) == 1) {
-			System.out.println("Ok response");
-			int num = rand.nextInt(7);
-			System.out.println(ok[num]);
-		}
-		
-		else {
-			System.out.println("Postitive response");
-			int num = rand.nextInt(10);
-			System.out.println(pos[num]);
-		}
 		
 	}
 	
